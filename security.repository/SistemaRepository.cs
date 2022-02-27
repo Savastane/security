5804952cca5f40c63fd17fc1a910b51ca87333cc
Savastane <savastane@gmail.com>
2022-02-27 14:10:03 -0300

    projeto repositorio

---
+++ security.repository/SistemaRepository.cs
@@ -0,0 +1,40 @@
+﻿
+using Raven.Client.Documents.Session;
+using Raven.Client.Documents;
+
+namespace site.repository.raven
+{
+    public class SistemaRepository /*: ISistemaRepository*/
+    {
+        private readonly IAsyncDocumentSession session;
+
+
+        public SistemaRepository(IAsyncDocumentSession dbSession)
+        {
+            this.session = dbSession;
+        }
+
+        public void ApplyScriptInicial()
+        {
+            
+        }
+
+        public void Backup()
+        {
+            var temp = $"http://localhost:8080{Guid.NewGuid()}";
+            //Directory.CreateDirectory(temp);
+
+            //Contexto.Store
+            //        .DatabaseCommands
+            //        .GlobalAdmin
+            //        .StartBackup(temp, new DatabaseDocument(), incremental: false, databaseName: "INEP")
+            //        .WaitForCompletion();
+
+            //var dir = $"http://localhost:8080{ DateTime.Now.ToString("yyyy/MM/dd/")}";
+            //var arquivoZip = $"{dir}{ DateTime.Now.ToString("HH-mm-ss")}.zip";
+            //Directory.CreateDirectory(dir);
+            //ZipFile.CreateFromDirectory(temp, arquivoZip);
+            Directory.Delete(temp, true);
+        }
+    }
+}
---
+++ security.repository/UserRepository.cs
@@ -0,0 +1,118 @@
+﻿
+
+
+namespace security.repository
+{
+    using security.domain;
+
+    using Raven.Client.Documents.Session;
+    using Raven.Client.Documents;
+
+
+    public class UserRepository : IUsuarioRepository
+    {
+        private string DataBaseName;
+        private  IAsyncDocumentSession session;
+
+        public UserRepository(IAsyncDocumentSession dbSession)
+        {
+            
+            this.session = dbSession;
+            this.session.Advanced.SetTransactionMode(TransactionMode.ClusterWide);
+         }            
+                
+        public Task<Usuario> GetById(string id)
+        {   
+                return session.LoadAsync<Usuario>("Usuarios/"+ id);
+        }
+
+        public Task<List<Usuario>> GetAll()
+        {
+
+            return session.Query<Usuario>()
+                   .OrderBy(x => x.Id)
+                   .ToListAsync();
+        
+        }
+
+        public Task<Usuario> Add(Usuario entidade)
+        {
+
+            if (DataBaseName!= session.Advanced.DocumentStore.Database)
+            {
+                session = session.Advanced.DocumentStore.OpenAsyncSession(DataBaseName);
+            }
+            
+
+                session.StoreAsync(entidade);
+                session.SaveChangesAsync();
+
+                return Task.FromResult(entidade);
+            
+        }
+
+        public void Remove(Usuario entidade)
+        {
+            
+                session.Delete(entidade.GetType() + "/" + entidade.Id);
+                session.SaveChangesAsync();
+
+                
+        }
+
+        public Task<Usuario> Update(Usuario entidade)
+        {
+            
+                session.StoreAsync(entidade);
+                session.SaveChangesAsync();
+
+            return Task.FromResult(entidade);
+        }
+               
+
+        public Task<Usuario> Authenticate(Usuario usuario)
+        {            
+            return GetByEmail(usuario.Email.Endereco);
+        }
+
+
+        public Task<Usuario> GetByEmail(string email)
+        {
+            var resultado = session.Query<Usuario>()
+                      .Where(x => x.Email.Endereco == email)
+                      .FirstOrDefaultAsync();
+
+            return resultado;
+        }
+
+        public void SetDatabase(string databasename)
+        {
+            //try (IDocumentStore store = new DocumentStore(new String[] { "http://localhost:8080" }, session.Advanced.DocumentStore.Database)) {
+            //    store.initialize();
+
+                
+            //}
+            //session.Advanced.DocumentStore.Database
+
+            //this.session = dbSession;
+            //this.session.Advanced.SetTransactionMode(TransactionMode.ClusterWide);
+
+            this.DataBaseName = databasename;
+
+            
+        }
+
+
+        //public void UpdateIdentificacao(string id, Identificacao identificacao)
+        //{
+
+        //    session.Advanced.Patch<Usuario, Identificacao>(
+        //                   $"usuarios/{id}" ,
+        //                   x => x.Identificacao, identificacao);
+
+        //    session.SaveChangesAsync();
+
+        //}
+
+    }
+}
